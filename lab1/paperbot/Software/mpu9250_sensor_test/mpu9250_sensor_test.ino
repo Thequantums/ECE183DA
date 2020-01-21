fde0201dcfd7e79ef126303fae20a668ede36f72
Thequantums <46539200+Thequantums@users.noreply.github.com>
2020-01-20 22:49:30 -0800

    Update mpu9250_sensor_test.ino
    Guaranteed reading mx, my, mz.

--- lab1/paperbot/Software/mpu9250_sensor_test/mpu9250_sensor_test.ino
+++ lab1/paperbot/Software/mpu9250_sensor_test/mpu9250_sensor_test.ino
@@ -76,7 +76,7 @@ void setup()
     I2Cread(MAG_ADDRESS,0x02,1,&ST1);
   }
   while (!(ST1&0x01));
-
+  Serial.print ("hello.\n");
   // Read magnetometer data  
   uint8_t Mag[7];  
   I2Cread(MAG_ADDRESS,0x03,7,Mag);
@@ -95,11 +95,10 @@ void setup()
    // 16 bits registers - 10.0f*4912.0f/8190.0f
    mRes = 10.0f * 4912.0f/8190.0f;
   
-  uint16_t ii = 0, sample_count = 0;
+  uint16_t ii = 0, sample_count;
   int16_t mag_max[3] = {-32767, -32767, -32767}, mag_min[3] = {32767, 32767, 32767};
 
   delay(4000);
-  
 // shoot for ~fifteen seconds of mag data
   /*
   if(_Mmode == 0x02) sample_count = 128;  // at 8 Hz ODR, new mag data is available every 125 ms
@@ -107,37 +106,40 @@ void setup()
   */ 
   //assume at 100 Hz ODR, new mag data is available every 10 ms
   sample_count = 1500;
+  uint8_t ST2;
   for(ii = 0; ii < sample_count; ii++) {
-    // :::  Magnetometer ::: 
-
-  // Request first magnetometer single measurement
-  I2CwriteByte(MAG_ADDRESS,0x0A,0x01);
+      // :::  Magnetometer ::: 
   
-  // Read register Status 1 and wait for the DRDY: Data Ready
+      // Request first magnetometer single measurement
+      I2CwriteByte(MAG_ADDRESS,0x0A,0x01);
   
-  uint8_t ST1;
-  do
-  {
-    I2Cread(MAG_ADDRESS,0x02,1,&ST1);
-  }
-  while (!(ST1&0x01));
+      // Read register Status 1 and wait for the DRDY: Data Ready
 
-  // Read magnetometer data  
-  uint8_t Mag_temp[7];  
-  I2Cread(MAG_ADDRESS,0x03,7,Mag_temp); // Read the x-, y-, and z-axis calibration values
+      do
+      {
+        I2Cread(MAG_ADDRESS,0x02,1,&ST2);
+    
+      }
+      while (!(ST2&0x01));
+
+      
+      // Read magnetometer data  
+      uint8_t Mag_temp[7];  
+      I2Cread(MAG_ADDRESS,0x03,7,Mag_temp); // Read the x-, y-, and z-axis calibration values
     
       // Magnetometer
-   int16_t mag_temp[3];    
-   mag_temp[0]=(Mag_temp[1]<<8 | Mag_temp[0]);
-   mag_temp[1]=(Mag_temp[3]<<8 | Mag_temp[2]);
-   mag_temp[2]=(Mag_temp[5]<<8 | Mag_temp[4]);
+      int16_t mag_temp[3];    
+      mag_temp[0]=(Mag_temp[1]<<8 | Mag_temp[0]);
+      mag_temp[1]=(Mag_temp[3]<<8 | Mag_temp[2]);
+      mag_temp[2]=(Mag_temp[5]<<8 | Mag_temp[4]);
     
-    for (int jj = 0; jj < 3; jj++) {
-      if(mag_temp[jj] > mag_max[jj]) mag_max[jj] = mag_temp[jj];
-      if(mag_temp[jj] < mag_min[jj]) mag_min[jj] = mag_temp[jj];
-    }
-    delay(100);
- }
+      for (int jj = 0; jj < 3; jj++) {
+        if(mag_temp[jj] > mag_max[jj]) mag_max[jj] = mag_temp[jj];
+        if(mag_temp[jj] < mag_min[jj]) mag_min[jj] = mag_temp[jj];
+      }
+      Serial.println("loop on going");
+      Serial.println(ii);
+  }
 
     Serial.println("mag x min/max:"); Serial.println(mag_max[0]); Serial.println(mag_min[0]);
     Serial.println("mag y min/max:"); Serial.println(mag_max[1]); Serial.println(mag_min[1]);
@@ -157,14 +159,29 @@ void setup()
     mag_scale[1]  = (mag_max[1] - mag_min[1])/2;  // get average y axis max chord length in counts
     mag_scale[2]  = (mag_max[2] - mag_min[2])/2;  // get average z axis max chord length in counts
 
-    float avg_rad = (mag_scale[0] + mag_scale[1] + mag_scale[2]) / 3.0;
+    float avg_rad = (mag_scale[0] + mag_scale[1] + mag_scale[2])/3.0;
 
     mag_scale[0] = avg_rad/((float)mag_scale[0]);
     mag_scale[1] = avg_rad/((float)mag_scale[1]);
     mag_scale[2] = avg_rad/((float)mag_scale[2]);
   
    Serial.println("Mag Calibration done!");
-  
+
+   Serial.print("mag_bias[0]:");
+   Serial.println(mag_bias[0]);
+   Serial.print("mag_bias[1]:");
+   Serial.println(mag_bias[1]);
+   Serial.print("mag_bias[2]:");
+   Serial.println(mag_bias[2]);
+   
+   Serial.print("mag_scale[0]:");
+   Serial.println(mag_scale[0]);
+      Serial.print("mag_scale[1]:");
+   Serial.println(mag_scale[1]);
+      Serial.print("mag_scale[2]:");
+   Serial.println(mag_scale[2]);
+
+   
   
 }
 
@@ -205,15 +222,19 @@ void loop()
   int16_t mx=(Mag[1]<<8 | Mag[0]);
   int16_t my=(Mag[3]<<8 | Mag[2]);
   int16_t mz=(Mag[5]<<8 | Mag[4]);
-  
+
+  int16_t uc_mx = mx;
+  int16_t uc_my = my;
+  int16_t uc_mz = mz;
   
   // 16 bits registers - 10.0f*4912.0f/8190.0f
   
       // Calculate the magnetometer values in milliGauss
     // Include factory calibration per data sheet and user environmental corrections
+    //*mRes*magCalibration[0]
       mx = (float)mx*mRes*magCalibration[0] - mag_bias[0];  // get actual magnetometer value, this depends on scale being set
-      my = (float)my*mRes*magCalibration[1] - mag_bias[1];  
-      mz = (float)mz*mRes*magCalibration[2] - mag_bias[2];  
+      my = (float)my*mRes*magCalibration[1]  - mag_bias[1];  
+      mz = (float)mz*mRes*magCalibration[2]  - mag_bias[2];  
       mx *= mag_scale[0];
       my *= mag_scale[1];
       mz *= mag_scale[2];
@@ -221,14 +242,15 @@ void loop()
   
   // arc tangent of y/x 
   float heading = atan2(mx, my);
-
+  float uc_heading = atan2(uc_mx, uc_my);
+  
   // Once you have your heading, you must then add your 'Declination Angle',
   // which is the 'Error' of the magnetic field in your location. Mine is 0.0404 
   // Find yours here: http://www.magnetic-declination.com/
   
   // If you cannot find your Declination, comment out these two lines, your compass will be slightly off.
-  float declinationAngle = 0.0404;
-  heading += declinationAngle;
+ // float declinationAngle = 0.0404;
+ // heading += declinationAngle;
 
   // Correct for when signs are reversed.
   if(heading < 0)
@@ -238,16 +260,32 @@ void loop()
   if(heading > 2*PI)
     heading -= 2*PI;
 
+      // Correct for when signs are reversed.
+  if(uc_heading < 0)
+    uc_heading += 2*PI;
+
+  // Check for wrap due to addition of declination.
+  if(uc_heading > 2*PI)
+    uc_heading -= 2*PI;
+
+
+
   // Convert radians to degrees for readability.
   float headingDegrees = heading * 180/PI; 
-
-  Serial.print("\rHeading:\t");
+  float uc_headingDegrees = uc_heading * 180/PI;
+  Serial.print("\rCalibrated Heading:\t");
   Serial.print(heading);
   Serial.print(" Radians   \t");
   Serial.print(headingDegrees);
   Serial.println(" Degrees   \t");
-
-  Serial.print ("Magnetometer readings:"); 
+  
+  Serial.print("\rUncalibrated Heading:\t");
+  Serial.print(uc_heading);
+  Serial.print(" Radians   \t");
+  Serial.print(uc_headingDegrees);
+  Serial.println(" Degrees   \t");
+  
+  Serial.print (" Calibrated Magnetometer readings:"); 
   Serial.print ("\tMx:");
   Serial.print (mx); 
   Serial.print ("\tMy:");
@@ -255,15 +293,19 @@ void loop()
   Serial.print ("\tMz:");
   Serial.print (mz);  
   Serial.println ("\t");
+
+  Serial.print("Uncalibrated Magnetometer reading: ");
+  Serial.print ("\tMx:");
+  Serial.print (uc_mx); 
+  Serial.print ("\tMy:");
+  Serial.print (uc_my);
+  Serial.print ("\tMz:");
+  Serial.print (uc_mz);  
+  Serial.println ("\t");
   
   
   // End of line
   delay(100); 
 }
-
-
-
-
-
 
 
