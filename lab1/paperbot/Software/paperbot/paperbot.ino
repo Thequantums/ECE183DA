082c3e84a45f5385845385e58c7ebcc56f587f4b
Thequantums <46539200+Thequantums@users.noreply.github.com>
2020-01-22 23:48:08 -0800

    Update paperbot.ino
    Merged Paperbot

--- lab1/paperbot/Software/paperbot/paperbot.ino
+++ lab1/paperbot/Software/paperbot/paperbot.ino
@@ -48,6 +48,38 @@
 #include "file.h"
 #include "server.h"
 
+// Include from mpu9250_sensor test
+//*****************************************************
+#include <Wire.h>
+
+#define    MPU9250_ADDRESS            0x68
+#define    MAG_ADDRESS                0x0C
+#define    GYRO_ADDRESS               0x43
+
+#define    GYRO_FULL_SCALE_250_DPS    0x00  
+#define    GYRO_FULL_SCALE_500_DPS    0x08
+#define    GYRO_FULL_SCALE_1000_DPS   0x10
+#define    GYRO_FULL_SCALE_2000_DPS   0x18
+
+#define    ACC_FULL_SCALE_2_G        0x00  
+#define    ACC_FULL_SCALE_4_G        0x08
+#define    ACC_FULL_SCALE_8_G        0x10
+#define    ACC_FULL_SCALE_16_G       0x18
+
+#define SDA_PORT 14
+#define SCL_PORT 12
+
+//calibrating variables
+float mRes, mag_scale[3] = {0,0,0}, mag_bias[3] = {0,0,0};
+uint8_t magCalibration[3] = {0,0,0};
+
+//*********************include from vl5310x_dual_sensor_test **********************************
+
+#include <VL53L0X.h>
+VL53L0X sensor;
+VL53L0X sensor2;
+
+//**************************************************************
 
 const int SERVO_LEFT = D1;
 const int SERVO_RIGHT = D2;
@@ -71,6 +103,184 @@ char* mDNS_name = "paperbot";
 
 String html;
 String css;
+
+//****************** from mpu9250_sensor_test *****************************************
+
+// This function read Nbytes bytes from I2C device at address Address. 
+// Put read bytes starting at register Register in the Data array. 
+void I2Cread(uint8_t Address, uint8_t Register, uint8_t Nbytes, uint8_t* Data)
+{
+  // Set register address
+  Wire.beginTransmission(Address);
+  Wire.write(Register);
+  Wire.endTransmission();
+  
+  // Read Nbytes
+  Wire.requestFrom(Address, Nbytes); 
+  uint8_t index=0;
+  while (Wire.available())
+    Data[index++]=Wire.read();
+}
+
+
+// Write a byte (Data) in device (Address) at register (Register)
+void I2CwriteByte(uint8_t Address, uint8_t Register, uint8_t Data)
+{
+  // Set register address
+  Wire.beginTransmission(Address);
+  Wire.write(Register);
+  Wire.write(Data);
+  Wire.endTransmission();
+}
+
+void readGyro_data(int16_t* raw_data ) {
+  
+  uint8_t temp_data [7];
+  Wire.beginTransmission(MPU9250_ADDRESS);
+  Wire.write(0x43);
+  Wire.endTransmission(false);
+  uint8_t i = 0;
+  Wire.requestFrom(MPU9250_ADDRESS,7);
+  while (Wire.available()) {
+      temp_data[i++] = Wire.read();
+  }
+   raw_data[0] = (temp_data[1]<<8 | temp_data[0]);
+   raw_data[1] = (temp_data[2]<<8 | temp_data[3]);
+   raw_data[2] = (temp_data[4]<<8 | temp_data[5]);
+  
+}
+
+// Initializations
+void setup_mpu9250()
+{
+  // Arduino initializations
+  Wire.begin(SDA_PORT,SCL_PORT);
+  Serial.begin(115200);
+
+  // Set by pass mode for the magnetometers
+  I2CwriteByte(MPU9250_ADDRESS,0x37,0x02);
+
+// ****** calibration code starts here **********
+// Note: Commented the calibration code out because we alreay got fixed calibrated value that can be plugged in
+/*
+  uint16_t ii = 0, sample_count;
+  int16_t mag_max[3] = {-32767, -32767, -32767}, mag_min[3] = {32767, 32767, 32767};
+
+  delay(1000);
+  
+  //assume at 100 Hz ODR, new mag data is available every 10 ms
+  sample_count = 300;
+  uint8_t ST2;
+  uint8_t Mag_temp[7];
+  
+  int16_t mag_temp[3] = {0,0,0};   
+  for(ii = 0; ii < sample_count; ii++) {
+      // :::  Magnetometer ::: 
+
+      // Request first magnetometer single measurement
+      I2CwriteByte(MAG_ADDRESS,0x0A,0x01);
+  
+      // Read register Status 1 and wait for the DRDY: Data Ready
+      do
+      {
+        I2Cread(MAG_ADDRESS,0x02,1,&ST2);
+      }
+      while (!(ST2&0x01));
+      
+      // Read magnetometer data  
+      I2Cread(MAG_ADDRESS,0x03,7,Mag_temp); // Read the x-, y-, and z-axis values
+    
+      //Magnetometer 
+      mag_temp[0]=(Mag_temp[1]<<8 | Mag_temp[0]);
+      mag_temp[1]=(Mag_temp[3]<<8 | Mag_temp[2]);
+      mag_temp[2]=(Mag_temp[5]<<8 | Mag_temp[4]);
+    
+      for (int jj = 0; jj < 3; jj++) {
+        if(mag_temp[jj] > mag_max[jj]) mag_max[jj] = mag_temp[jj];
+        if(mag_temp[jj] < mag_min[jj]) mag_min[jj] = mag_temp[jj];
+      }
+      Serial.println("Calibrating");
+      Serial.println(ii);
+      delay(135);
+  }
+
+
+    // Get hard iron correction
+    mag_bias[0]  = (mag_max[0] + mag_min[0])/2;  // get average x mag bias in counts
+    mag_bias[1]  = (mag_max[1] + mag_min[1])/2;  // get average y mag bias in counts
+    mag_bias[2]  = (mag_max[2] + mag_min[2])/2;  // get average z mag bias in counts
+       
+    // Get soft iron correction estimate
+    mag_scale[0]  = (mag_max[0] - mag_min[0])/2;  // get average x axis max chord length in counts
+    mag_scale[1]  = (mag_max[1] - mag_min[1])/2;  // get average y axis max chord length in counts
+    mag_scale[2]  = (mag_max[2] - mag_min[2])/2;  // get average z axis max chord length in counts
+
+    float avg_rad = (mag_scale[0] + mag_scale[1] + mag_scale[2])/3.0;
+
+    mag_scale[0] = avg_rad/((float)mag_scale[0]);
+    mag_scale[1] = avg_rad/((float)mag_scale[1]);
+    mag_scale[2] = avg_rad/((float)mag_scale[2]);
+  
+   Serial.println("Mag Calibration done!");
+   */
+}
+
+//************************set up for vl530x*************
+void setup_vl53l0 () {
+  
+  pinMode(D3, OUTPUT);
+  pinMode(D4, OUTPUT);
+  digitalWrite(D7, LOW);
+  digitalWrite(D8, LOW);
+
+  delay(500);
+  digitalWrite(D3, HIGH);
+  delay(150);
+  Serial.println("00");
+  
+  sensor.init(true);
+  Serial.println("01");
+  delay(100);
+  sensor.setAddress((uint8_t)22);
+
+  digitalWrite(D4, HIGH);
+  delay(150);
+  sensor2.init(true);
+  Serial.println("03");
+  delay(100);
+  sensor2.setAddress((uint8_t)25);
+  Serial.println("04");
+
+  Serial.println("addresses set");
+
+  Serial.println ("I2C scanner. Scanning ...");
+  byte count = 0;
+
+  for (byte i = 1; i < 120; i++)
+  {
+
+    Wire.beginTransmission (i);
+    if (Wire.endTransmission () == 0)
+    {
+      Serial.print ("Found address: ");
+      Serial.print (i, DEC);
+      Serial.print (" (0x");
+      Serial.print (i, HEX);
+      Serial.println (")");
+      count++;
+      delay (1);  // maybe unneeded?
+    } // end of good response
+  } // end of for loop
+  Serial.println ("Done.");
+  Serial.print ("Found ");
+  Serial.print (count, DEC);
+  Serial.println (" device(s).");
+
+  delay(3000);  
+
+}
+
+
 
 void setup() {
     setupPins();
@@ -99,11 +309,111 @@ void setup() {
     setupHTTP();
     setupWS(webSocketEvent);
     //setupMDNS(mDNS_name);
+    
+    //set up mpu 9250
+    setup_mpu9250();
+
+    //set up vl53l0 sensor
+    setup_vl53l0();
 
     stop();
 }
 
 void loop() {
+  
+   // Request first magnetometer single measurement
+  I2CwriteByte(MAG_ADDRESS,0x0A,0x01);
+  
+  // Read register Status 1 and wait for the DRDY: Data Ready
+  
+  uint8_t ST1;
+  do
+  {
+    I2Cread(MAG_ADDRESS,0x02,1,&ST1);
+  }
+  while (!(ST1&0x01));
+
+  // Read magnetometer data  
+  uint8_t Mag[7];  
+  I2Cread(MAG_ADDRESS,0x03,7,Mag);
+
+  // Create 16 bits values from 8 bits data
+  
+  // Magnetometer
+  int16_t mx=(Mag[1]<<8 | Mag[0]);
+  int16_t my=(Mag[3]<<8 | Mag[2]);
+  int16_t mz=(Mag[5]<<8 | Mag[4]);
+
+  
+  // 16 bits registers - 10.0f*4912.0f/8190.0f
+  
+      // Calculate the magnetometer values in milliGauss
+    // Include factory calibration per data sheet and user environmental corrections
+    //*mRes*magCalibration[0]
+   float f_mx = (float)mx - 64;
+   float f_my = (float)my + 162.6;
+   float f_mz = (float)mz + 1;
+    f_mx *= 1.012;
+    f_my *= 1.01;
+    f_mz *= 0.978;
+    
+  // arc tangent of y/x 
+  float heading = atan2(f_mx, f_my);
+
+    // Correct for when signs are reversed.
+  if(heading < 0)
+    heading += 2*PI;
+
+  // Check for wrap due to addition of declination.
+  if(heading > 2*PI)
+    heading -= 2*PI;
+
+  // Convert radians to degrees for readability.
+  float headingDegrees = heading * 180/PI; 
+  
+  //float uc_headingDegrees = uc_heading * 180/PI;
+  Serial.print("\rCalibrated Heading:\t");
+  Serial.print(heading);
+  Serial.print(" Radians   \t");
+  Serial.print(headingDegrees);
+  Serial.println(" Degrees   \t");
+  
+  Serial.print (" Calibrated Magnetometer readings:"); 
+  Serial.print ("\tMx:");
+  Serial.print (f_mx*0.6); 
+  Serial.print ("\tMy:");
+  Serial.print (f_my*0.6);
+  Serial.print ("\tMz:");
+  Serial.print (f_mz*0.6);  
+  Serial.println ("\t");
+
+
+//**************** Gyro Reading *****************
+  // Set by pass mode for the Gyro
+  int16_t gyro_data[3];
+  readGyro_data(gyro_data);
+  Serial.print("Gyro data:  ");
+  Serial.print ((float)((gyro_data[0] * 250) / 32768));  
+  Serial.print ("\t");
+  Serial.print ((float)((gyro_data[1] * 250) / 32768));  
+  Serial.print ("\t");
+  Serial.print ((float)((gyro_data[2] * 250) / 32768));  
+  Serial.print ("\t");
+  Serial.println("");
+  
+    // End of line
+    
+//*********** loop for vl
+  Serial.print("Lidar 1 range(mm): ");
+  Serial.print(sensor.readRangeSingleMillimeters());
+  if (sensor.timeoutOccurred()) { Serial.print(" TIMEOUT"); }
+  
+  Serial.print("  Lidar 2 range(mm): ");
+  Serial.println(sensor2.readRangeSingleMillimeters());
+  if (sensor.timeoutOccurred()) { Serial.println(" TIMEOUT"); }
+    
+
+// *************** web sever loop
     wsLoop();
     httpLoop();
 }
