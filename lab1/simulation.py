3332fa852fb832d9fd7ad61539aa788195ef892f
Rek'Sai2 <swingly@ucla.edu>
2020-01-22 22:08:16 -0800

    Fully Commented, Just waiting for the loop

--- lab1/simulation.py
+++ lab1/simulation.py
@@ -19,8 +19,8 @@ A = 530          # X ceiling(mm)
 B = 400          # Y ceiling(mm)
 T = .1           # Sampling Period(s)
 M = 25           # Magnetometer magnitude (+-6)(uT)
-R = 90           # Radius of Wheel (mm)
-L = 180          # Length of Robot (mm)
+R = 50           # Radius of Wheel (mm)
+L = 90           # Width of Robot (mm)
 
 # Global values for critical angles
 # NOTE: A critical angle is the angle of a line from the cars position
@@ -36,8 +36,8 @@ theta_4 = 0      # Critical angle 4 (rad)
 w_curve = 0      # Angular Velocity for State Dynamics
 
 # Global value for STATE
-x = 0            # X coord
-y = 0            # Y coord
+x = 265            # X coord
+y = 200            # Y coord
 theta = 0        # Heading
 
 # Global value for NEXT STATE
@@ -85,7 +85,7 @@ def state_dynamics(vl, vr):
     global theta_next
     if vl != vr:  # If the robot is following a curved path
         r_curve = L*(vl+vr)/(2*(vr-vl))  # calculate radius of curvature
-        w_curve = (vr-vl)*L              # calculate angular velocity
+        w_curve = (vr-vl) / L              # calculate angular velocity
         ccx = x - r_curve*math.sin(theta)   # calculate center of curvature (x coord)
         ccy = y + r_curve*math.cos(theta)   # calculate center of curvature (y coord)
         x_next = (x - ccx)*math.cos(w_curve*T)-(y-ccy)*math.sin(w_curve*T) + ccx    # calc next x coord
@@ -110,57 +110,57 @@ def state_checker(vl, vr):
     global x_next
     global y_next
     global theta_next
-    if (x_next < 0 or x_next > A) and (y_next < 0 or y_next > B):  # Both wheels stuck, stay in current state
+    if (x_next <= 0 or x_next >= A) and (y_next <= 0 or y_next >= B):  # Both wheels stuck, stay in current state
         x_next = x
         y_next = y
         theta_next = theta
-    elif x_next < 0 or x_next > A:  # Error along x-axis
+    elif x_next <= 0 or x_next >= A:  # Error along x-axis
         if theta < math.pi/2:
             state_dynamics(vl, 0)  # Pivot along right-wheel
-            if y_next < 0 or y_next > B:  # If both wheels stuck, stay in current state
+            if y_next <= 0 or y_next >= B:  # If both wheels stuck, stay in current state
                 x_next = x
                 y_next = y
                 theta_next = theta
         elif theta < math.pi:
             state_dynamics(0, vr)  # Pivot along left-wheel
-            if y_next < 0 or y_next > B:  # If both wheels stuck, stay in current state
+            if y_next <= 0 or y_next >= B:  # If both wheels stuck, stay in current state
                 x_next = x
                 y_next = y
                 theta_next = theta
         elif theta < 3*math.pi/2:
             state_dynamics(vl, 0)  # Pivot along right-wheel
-            if y_next < 0 or y_next > B:  # If both wheels stuck, stay in current state
+            if y_next <= 0 or y_next >= B:  # If both wheels stuck, stay in current state
                 x_next = x
                 y_next = y
                 theta_next = theta
         else:
             state_dynamics(0, vr)  # Pivot along left-wheel
-            if y_next < 0 or y_next > B:  # If both wheels stuck, stay in current state
+            if y_next <= 0 or y_next >= B:  # If both wheels stuck, stay in current state
                 x_next = x
                 y_next = y
                 theta_next = theta
-    elif y_next < 0 or y_next > B:  # Error along x-axis
+    elif y_next <= 0 or y_next >= B:  # Error along x-axis
         if theta < math.pi/2:
             state_dynamics(0, vr)  # Pivot along left-wheel
-            if x_next < 0 or x_next > A:  # If both wheels stuck, stay in current state
+            if x_next <= 0 or x_next >= A:  # If both wheels stuck, stay in current state
                 x_next = x
                 y_next = y
                 theta_next = theta
         elif theta < math.pi:
             state_dynamics(vl, 0)  # Pivot along right-wheel
-            if x_next < 0 or x_next > A:  # If both wheels stuck, stay in current state
+            if x_next <= 0 or x_next >= A:  # If both wheels stuck, stay in current state
                 x_next = x
                 y_next = y
                 theta_next = theta
         elif theta < 3*math.pi/2:
             state_dynamics(0, vr)  # Pivot along left-wheel
-            if x_next < 0 or x_next > A:  # If both wheels stuck, stay in current state
+            if x_next <= 0 or x_next >= A:  # If both wheels stuck, stay in current state
                 x_next = x
                 y_next = y
                 theta_next = theta
         else:
             state_dynamics(vl, 0)  # Pivot along right-wheel
-            if x_next < 0 or x_next > A:  # If both wheels stuck, stay in current state
+            if x_next <= 0 or x_next >= A:  # If both wheels stuck, stay in current state
                 x_next = x
                 y_next = y
                 theta_next = theta
@@ -176,16 +176,14 @@ def critical():  # Determines Critical Angles
     global A
     global x
     global y
-    global theta
-    global M
     global theta_1
     global theta_2
     global theta_3
     global theta_4
     theta_1 = math.atan2(B-y, A-x)  # Angle to the top-right corner
-    theta_2 = math.atan2(B-y, -x) + math.pi  # Angle to the top-left corner
-    theta_3 = math.atan2(y, x) + math.pi # Angle to the bottom-left corner
-    theta_4 = math.atan2(-y, A-x) # Angle to the bottom-right corner
+    theta_2 = math.atan2(B-y, -x)  # Angle to the top-left corner
+    theta_3 = math.atan2(-y, -x) + 2*math.pi  # Angle to the bottom-left corner
+    theta_4 = math.atan2(-y, A-x) + 2*math.pi  # Angle to the bottom-right corner
     return
 
 
@@ -197,25 +195,25 @@ def the_d(special_theta):
     global A
     global B
     global theta
-    if special_theta < theta_1:  # Hitting right wall
+    if special_theta <= theta_1:  # Hitting right wall
         d = (A-x)/math.cos(special_theta)
-    elif special_theta < theta_2:  # Hitting top wall
+    elif special_theta <= theta_2:  # Hitting top wall
         if special_theta <= math.pi/2:
             d = (B - y)/math.sin(special_theta)
         else:
-            d = (B - y)/math.sin(special_theta - math.pi/2)
-    elif special_theta < theta_3:  # Hitting left wall
+            d = (B - y)/math.sin(math.pi - special_theta)
+    elif special_theta <= theta_3:  # Hitting left wall
         if special_theta <= math.pi:
             d = x / math.cos(math.pi - special_theta)
         else:
             d = x / math.cos(special_theta - math.pi)
-    elif special_theta < theta_4:  # Hitting bottom wall
+    elif special_theta <= theta_4:  # Hitting bottom wall
         if special_theta <= 3*math.pi/2:
             d = y / math.sin(special_theta - math.pi)
         else:
-            d = y / math.cos(special_theta - 3*math.pi/2)
+            d = y / math.sin(2*math.pi - special_theta)
     else:  # Hitting right wall again
-        d = (A-x)/math.cos(2*math.pi-special_theta)
+        d = (A-x)/math.cos(2*math.pi - special_theta)
     return d
 
 
@@ -235,55 +233,51 @@ def main():
     i = 0  # Run a counter during each iteration to get timestamp
     # Code in between #'s should be in a loop
     ###################################################################################################
-    myfile = open("pwm_data.txt",'r') # file to read
-    wr_file = open('simulation_data.txt', 'w') # file to write
+
+    myfile = open("pwm_data.txt", 'r')  # file to read
+    wr_file = open('simulation_data.txt', 'w')  # file to write
     wr_file.write("pwml   pwmr   d1   d2   mx   my   gyro   timestamp\n")
 
     for line in myfile:
-       pwml_str ='';
-       pwmr_str = '';
-       s_begin = 0;
-       for letter in line:
-         if letter != ',' and s_begin == 0:
-            pwml_str = pwml_str + letter
-         elif letter != ',' and s_begin ==1:
-            pwmr_str = pwmr_str + letter;
-         else:
-            s_begin = 1
-       pwml = (float)(pwml_str)
-       pwmr = (float)(pwmr_str)
-       #############################3###########3
-       v_left = pwm_to_velocity(pwml)  # Get left speed
-       v_right = pwm_to_velocity(pwmr)  # Get right speed
-       state_dynamics(v_left, v_right)  # Update state
-       state_checker(v_left, v_right)  # Check validity of update
-       critical()  # Calculate critical values
-       d1 = the_d(theta)  # Distance of forward laser, orientation of laser is same as car
-       theta_x = theta - math.pi/2  # Orientation of second laser is -90 degrees of car's     orientation
-       if theta_x < 0:  # Correct orientation for below 0 degrees and above 360 degrees
-         theta_x = theta_x + math.pi*2
-       elif theta_x > math.pi*2:
-         theta_x = theta_x - math.pi*2
-       d2 = the_d(theta_x)  # Distance of side laser
-       mx = M*math.sin(theta)  # Magnetometer along x-axis
-       my = M*math.cos(theta)  # Magnetometer along y-axis
-       gyro = w_curve  # Gyro Reading
-       timestamp = T*i
-       ##############################################
-       #  write out values d1,d2,mx,my,gyro, and timestamp
-       #rounded to 2 decimal places values
-       wr_file.write(str(round(pwml,2)) + ', ' + str(round(pwmr,2)) + ', ' + str(round(d1,2)) + ', ' + str(round(d2,2)) + ', ' + str(round(mx,2)) + ', ' + str(round(my,2)) + ', ' + str(round(gyro,2)) + ', ' + str(round(timestamp,2)) + "\n")
-       
-       # Update state
-       x = x_next
-       y = y_next
-       theta = theta_next
-       i = i+1
-
+        pwml_str = ''
+        pwmr_str = ''
+        s_begin = 0
+        for letter in line:
+            if letter != ',' and s_begin == 0:
+                pwml_str = pwml_str + letter
+            elif letter != ',' and s_begin == 1:
+                pwmr_str = pwmr_str + letter
+            else:
+                s_begin = 1
+        pwml = float(pwml_str)
+        pwmr = float(pwmr_str)
+        v_left = pwm_to_velocity(pwml)  # Get left speed
+        v_right = pwm_to_velocity(pwmr)  # Get right speed
+        state_dynamics(v_left, v_right)  # Update state
+        state_checker(v_left, v_right)  # Check validity of update
+        critical()  # Calculate critical values
+        d1 = the_d(theta)  # Distance of forward laser, orientation of laser is same as car
+        theta_x = theta - math.pi / 2  # Orientation of second laser is -90 degrees of car's orientation
+        if theta_x < 0:  # Correct orientation for below 0 degrees and above 360 degrees
+            theta_x = theta_x + math.pi * 2
+        elif theta_x > math.pi * 2:
+            theta_x = theta_x - math.pi * 2
+        d2 = the_d(theta_x)  # Distance of side laser
+        mx = M * math.sin(theta)  # Magnetometer along x-axis
+        my = M * math.cos(theta)  # Magnetometer along y-axis
+        gyro = w_curve  # Gyro Reading
+        timestamp = T * i
+        #  Print out values d1,d2,mx,my,gyro, and timestamp
+        wr_file.write(str(round(pwml, 2)) + ', ' + str(round(pwmr, 2)) + ', ' + str(round(d1, 2)) + ', '
+                      + str(round(d2, 2)) + ', ' + str(round(mx, 2)) + ', ' + str(round(my, 2))
+                      + ', ' + str(round(gyro, 2)) + ', ' + str(round(timestamp, 2)) + "\n")
+        # Update state
+        x = x_next
+        y = y_next
+        theta = theta_next
+        i = i + 1
     #####################################################################################################
 
-    myfile.close()
-    wr_file.close()    
 
 if __name__ == "__main__":
     main()
