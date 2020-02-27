921008a0129308c82d48d08a2dbc33003dc44737
coopermssimpson <cmssimpson@me.com>
2020-02-26 17:27:49 -0800

    Began Lab 3

---
+++ lab3/RRT.py
@@ -0,0 +1,107 @@
+import math
+import random
+import matplotlib.pyplot as plt
+import numpy as np
+class rrt():
+
+
+    def __init__(self, origin = [250, 0, 0], maxcoords = [500,500], stepsize = 5, N = 10000, obstacles = [[0, 0, 100, 500], [400, 0, 500, 500], [200, 300, 400, 325],
+                     [100, 350, 250, 375]], goal = [140, 400, 150, 410]):
+        self.origin = origin  # Origin point, in form x,y,parent
+        self.maxcoords = maxcoords  # Max values of field. x,y form. Assumes bottom left is 0,0
+        self.stepsize = stepsize  # size of step to take (1=> unit vector step)
+        self.N = N  # Iterations to run
+        self.obstacles = obstacles  # obstacle list. Rectangles only, in form xmin, ymin, xmax, ymax
+        self.goal = goal  # goal. Rectangles only, in form xmin, ymin, xmax, ymax
+        self.nodesList = [origin]  # list of all nodes
+
+    def finddist(self,node1, node2):  # returns the euclidian distance between two nodes
+        dist = math.sqrt(pow(node1[0] - node2[0], 2) + pow(node1[1] - node2[1], 2))
+        return dist
+
+    def randomPoint(self):  # generates a random point
+        global maxcoords
+        point = [random.randint(0, self.maxcoords[0]), random.randint(0, self.maxcoords[1])]
+        return point
+
+    def obsCheck(self, point, obstacles):  # checks if the point is inside an obstacle. if it is, returns the origin. ##FUTURE## return the closest allowed point
+        for o in obstacles:
+            if ((o[0] < point[0] < o[2]) and (o[1] < point[1] < o[3])):
+                return [self.origin[0], self.origin[1]]
+        return point
+
+    def takestep(self, startnode, targetnode, nodes):  # finds a point one unit step from startnode, in the direction of targetnode. Takes "node" in order to set new node's parent node in node[2]
+        if startnode != targetnode:
+            dist = self.finddist(startnode, targetnode)
+            newx = ((targetnode[0] - startnode[0]) / dist) * self.stepsize
+            newy = ((targetnode[1] - startnode[1]) / dist) * self.stepsize
+            checkednode = self.obsCheck([newx + startnode[0], newy + startnode[1], nodes.index(startnode)], self.obstacles)
+        else:
+            checkednode = startnode
+        return checkednode
+
+    def findclosest(self,nodes, newnode):  # finds the closest node to newnode in nodelist nodes
+        distances = []
+        for i in nodes:
+            distances.append(self.finddist(i, newnode))
+        return nodes[distances.index(min(distances))]
+
+    def checkgoal(self,nodelist, node, goal):  # checks if the point is within the goal. if not, it sets goalfound to false. if it is, it returns the node path it took and sets goalfound to false
+        goalpath = []
+        tracenode = []
+
+        if (goal[0] < node[0] < goal[2] and goal[1] < node[1] < goal[3]):
+            goalfound = True
+            tracenode = node
+            goalpath.append(tracenode)
+            while (tracenode[2] != 0):
+                tracetemp = nodelist[tracenode[2]]
+                goalpath.append(tracetemp)
+                tracenode = tracetemp
+        else:
+            goalfound = False
+        goalpath.append(self.origin)
+        return [goalfound, goalpath]
+
+    def initplot(self, goal, obstacles):  # initializes plot by drawing obstacles, goal, and origin as well as setting the axes
+        goalbox = [[goal[0], goal[2], goal[2], goal[0], goal[0]], [goal[1], goal[1], goal[3], goal[3], goal[1]]]
+        for o in obstacles:
+            obsbox = [[o[0], o[2], o[2], o[0], o[0]], [o[1], o[1], o[3], o[3], o[1]]]
+            plt.plot(obsbox[0], obsbox[1], 'r')
+        plt.plot(goalbox[0], goalbox[1], 'g')
+        plt.xlim(0, self.maxcoords[0])
+        plt.ylim(0, self.maxcoords[1])
+
+    def optimize(self):
+        pass
+        # dumb stuff
+
+    def rrt(self, verbose = False, plotting = False):
+        self.initplot(self.goal, self.obstacles)
+        for k in range(0, self.N):
+            xrand = self.randomPoint()
+            xnear = self.findclosest(self.nodesList, xrand)
+            xnew = self.takestep(xnear, xrand, self.nodesList)
+            [goalbool, goalpath] = self.checkgoal(self.nodesList, xnew, self.goal)
+            if (goalbool):
+                [xg, yg, zg] = list(zip(*goalpath))
+                if verbose == True:
+                    print('PATH FOUND')
+                break
+            else:
+                self.nodesList.append(xnew)
+            if verbose == True:
+                print(k)
+
+        x, y = list(zip(*self.nodesList))
+        if plotting == True:
+            if goalbool:
+                plt.plot(xg, yg, 'y')
+            plt.scatter(x, y, s=1)
+            plt.show()
+        return [xg, yg]
+
+
+
+
+
---
+++ lab3/lab3.py
@@ -0,0 +1,4 @@
+import RRT
+
+r = RRT.rrt(stepsize = 10,N = 10000)
+r.rrt(verbose=True,plotting=True)
